bc3097aa300bf584b2e92dd18198cbcd433b52ff
Donadon <andreltdonadon@gmail.com>
2024-08-23 17:02:50 -0300

    Create static list

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './sales-person-list.component.css'
 })
 export class SalesPersonListComponent {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@email.com', 50000),
+    new SalesPerson('John', 'Doe', 'john.doe@email.com', 40000),
+    new SalesPerson('Claire', 'Murphy', 'claire@email.com', 90000),
+    new SalesPerson('Mai', 'Truong', 'mai@email.com', 60000)
+  ];
 
 }
